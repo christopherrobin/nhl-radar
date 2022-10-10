e9952ba8355f89537a51d2b36680c2ecc803115f
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2022-10-10 02:15:12 -0400

    Use next router so we retain props after team selection click

--- components/TeamSelectionCard.js
+++ components/TeamSelectionCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 
 const TeamSelectionCard = ({team, theme}) => {
+  const router = useRouter();
   const { id, name } = team;
   const imageMode = theme.palette.mode === 'dark' ? 'light' : 'dark';
   const imageURL = `https://www-league.nhlstatic.com/nhl.com/builds/site-core/d1b262bacd4892b22a38e8708cdb10c8327ff73e_1579810224/images/logos/team/current/team-${id}-${imageMode}.svg`;
@@ -17,7 +19,7 @@ const TeamSelectionCard = ({team, theme}) => {
             md: 250
           }
         }}>
-        <CardActionArea href={`/team/${id}`}>
+        <CardActionArea onClick={() => router.push(`/team/${id}`)}>
           <CardMedia
             component="img"
             height="200"
